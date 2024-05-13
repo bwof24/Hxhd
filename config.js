c1bd332408f73c13d4a6c3ef77148ec300f6050f
bwof24 <134535566+bwof24@users.noreply.github.com>
2024-05-13 18:53:45 +0600

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+// config.js
+module.exports = {
+   botToken: "6772706755:AAFFdyEkY_II54Xdm3Pkn6CuqUNckIL7oqQ", // Modify your bot TOKEN here
+   hostURL: "localhost:5000", // Modify your host URL here
+};
+// for port forewording use "ssh -R 80:localhost:5000 serveo.net"
