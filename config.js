bda46bad1190adf3d56a611195b4a1df132890f6
bwof24 <134535566+bwof24@users.noreply.github.com>
2024-05-14 15:55:09 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,19 @@
-// config.js
-module.exports = {
-   botToken: "6772706755:AAFFdyEkY_II54Xdm3Pkn6CuqUNckIL7oqQ", // Modify your bot TOKEN here
-   hostURL: "localhost:5000", // Modify your host URL here
-};
-// for port forewording use "ssh -R 80:localhost:5000 serveo.net"
+const { exec } = require('child_process');
+
+// Run the SSH command
+exec('ssh -R 80:localhost:5000 serveo.net', (error, stdout, stderr) => {
+  if (error) {
+    console.error(`Error executing command: ${error}`);
+    return;
+  }
+  const regex = /Forwarding\s*(http:\/\/.*)\s*-> localhost:5000/;
+  const match = stdout.match(regex);
+  const hostURL = match ? match[1] : "localhost:5000";
+  console.log(`Host URL: ${hostURL}`);
+
+  // Use the extracted hostURL value
+  module.exports = {
+    botToken: "7136354947:AAFSqq7Ayc6v9Ns9isbyn6K0yCeHAsyePao", // Modify your bot TOKEN here
+    hostURL: hostURL,
+  };
+});
